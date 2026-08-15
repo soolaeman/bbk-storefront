62dda75968e2d2d7962ba24ba9a56fd66b41c630
soolaemanwork-gif <soolaeman.work@gmail.com>
2026-08-16 00:02:24 +0700

    Make homepage product grid 2 columns on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -351,7 +351,7 @@ export default function App() {
           </div>
         ) : products.length > 0 ? (
           <>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+            <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 sm:gap-5">
               {products.map((product) => (
                 <ProductCard
                   key={product.id}

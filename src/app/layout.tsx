bd049052e6649a40a118e86b40402c2c846b9f35
soolaeman <soolaeman.work@gmail.com>
2026-08-17 20:55:00 +0700

    fix: point metadata to new favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,10 +25,6 @@ export const metadata: Metadata = {
     index: true,
     follow: true,
   },
-  icons: {
-    icon: '/BBKitchen.ico',
-    shortcut: '/BBKitchen.ico',
-  },
   openGraph: {
     type: 'website',
     locale: 'id_ID',

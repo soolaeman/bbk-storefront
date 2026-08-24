ee77d023843f9ffdbd89d792dc1f446a063ffc72
soolaeman <soolaeman.work@gmail.com>
2026-08-24 12:31:13 +0700

    fix: correct Dapur MBG landing import path

--- src/app/solusi-peralatan-dapur-mbg/page.tsx
+++ src/app/solusi-peralatan-dapur-mbg/page.tsx
@@ -1,5 +1,5 @@
-import DapurMbgPage from '../../dapur-mbg/page';
+import DapurMbgPage from '../dapur-mbg/page';
 
-export { metadata } from '../../dapur-mbg/page';
+export { metadata } from '../dapur-mbg/page';
 
 export default DapurMbgPage;

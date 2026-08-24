9a4349b64adc7dc751e7107ce28dff141f722f8d
soolaeman <soolaeman.work@gmail.com>
2026-08-24 11:31:39 +0700

    seo: redirect catalog to katalog

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/catalog',
+        destination: '/katalog',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
